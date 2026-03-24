48c6a52a902c8fbaf34b79c693bb1496d83a463d
subb-2 <zhsks312@gmail.com>
2026-03-24 20:13:38 +0900

    before add bus

---
+++ 0324_rv32i_mcu/0324_rv32i_mcu.srcs/sources_1/new/APB_Master.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/03/24 19:52:10
+// Design Name: 
+// Module Name: APB_Master
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module APB_Master(
+
+    );
+endmodule
